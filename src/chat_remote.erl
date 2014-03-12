a7b9bd5b66b2169ca660120b716407302922446b
Rafal Studnicki <rafal@opencubeware.org>
2014-03-12 11:07:48 +0000

    Add still preview grabber

--- src/chat_remote.erl
+++ src/chat_remote.erl
@@ -28,36 +28,30 @@ init([]) ->
     {ok, #state{}}.
 
 handle_call({start, Bitrate, Output}, _From, State) ->
-    Reply = chat_camera:start(Bitrate, Output),
+    Reply = {ok, chat_camera:start(Bitrate, Output)},
     {reply, Reply, State};
 handle_call(stop, _From, State) ->
-    Reply = chat_camera:stop(),
+    Reply = {ok, chat_camera:stop()},
     {reply, Reply, State};
+handle_call(preview, _From, State) ->
+    {reply, handle_preview(), State};
 handle_call(add_logo, _From, State) ->
-    Reply = chat_overlay:add_logo(logo),
+    Reply = {ok, chat_overlay:add_logo(logo)},
     {reply, Reply, State};
 handle_call({add_info, Competition, Event, Info}, _From, State) ->
-    Reply = chat_overlay:add_info(Event, Competition, Info),
+    Reply = {ok, chat_overlay:add_info(Event, Competition, Info)},
     {reply, Reply, State};
 handle_call({delete_segment, Id}, _From, State) ->
-    Reply = chat_overlay:delete_segment(Id),
+    Reply = {ok, chat_overlay:delete_segment(Id)},
     {reply, Reply, State};
 handle_call(delete_segments, _From, State) ->
-    Reply = chat_overlay:delete_segments(),
+    Reply = {ok, chat_overlay:delete_segments()},
     {reply, Reply, State};
 handle_call(status, _From, State) ->
-    Reply = try
-        {running,
-         [{bitrate, chat_camera:get_bitrate()},
-          {segments, chat_overlay:segments()}]}
-    catch exit:{noproc,_} ->
-        not_running
-    end,
+    Reply = {ok, handle_status()},
     {reply, Reply, State};
 handle_call(metrics, _From, State) ->
-    Fps = folsom_metrics:get_histogram_statistics(fps),
-    Starts = folsom_metrics:get_metric_value(camera_starts),
-    Reply = [{fps, Fps}, {camera_starts, Starts}],
+    Reply = {ok, handle_metrics()},
     {reply, Reply, State};
 handle_call(_Request, _From, State) ->
     Reply = ok,
@@ -78,3 +72,32 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+handle_preview() ->
+    Cmd = lists:concat(["raspistill -w 640 -h 480 "
+                        " -q 100 -t 1 -vf -hf -o -"]),
+    Port = erlang:open_port({spawn, Cmd}, [binary, exit_status]),
+    capture_preview(<<>>, Port).
+
+capture_preview(Acc, Port) ->
+    receive
+        {Port, {data, Data}} ->
+            capture_preview(<<Acc/binary, Data/binary>>, Port);
+        {Port, {exit_status, 0}} ->
+            {ok, Acc};
+        {Port, {exit_status, _}} ->
+            {error, unknown}
+    end.
+
+handle_status() ->
+    try
+        {running,
+         [{bitrate, chat_camera:get_bitrate()},
+          {segments, chat_overlay:segments()}]}
+    catch exit:{noproc,_} ->
+            not_running
+    end.
+
+handle_metrics() ->
+    Fps = folsom_metrics:get_histogram_statistics(fps),
+    Starts = folsom_metrics:get_metric_value(camera_starts),
+    [{fps, Fps}, {camera_starts, Starts}].
